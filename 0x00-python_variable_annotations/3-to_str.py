d843a811c0a9e65385e72f57be8d94229687a0d7
arkoaikins <arkoaikins@gmail.com>
2024-01-11 10:40:02 -0500

    write function that returns string rep of the float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""a type-annotated function floor which takes
+a float n as argument and returns the floor of the float."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of the float"""
+    return str(n)
